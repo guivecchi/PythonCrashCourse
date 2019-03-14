2c2d13e03613178551111b87671919cfb4958df2
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-03-14 13:40:21 -0300

    Add chapter 2 file with examples

---
+++ chapter2_examples.py
@@ -0,0 +1,17 @@
+first_name = "python"
+last_name = "course"
+name = first_name + " " + last_name
+print("\tThis is a great " + name.title() + "!")
+print("This is a great " + name.upper() + "!")
+print("This is a great \n" + name.lower() + "\n!\n\n")
+
+title = "   whitespace   "
+print(title.rstrip() + "!")
+print(title.lstrip() + "!")
+print(title.strip() + "!\n\n")
+
+# Exponential is denoted by ** - Ex: 2**3 = 8
+
+age = 22
+
+print("Happy " + str(age) + "nd Birthday!")
