535619156837d2a67380f3867f0ecc97cbb2b292
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-03-18 16:52:47 -0300

    Add Chapter 7 examples

---
+++ chapter7_examples.py
@@ -0,0 +1,106 @@
+## Chapter about user input and while loops
+
+# message = input("Tell me something, and I will repeat it back to you: ")
+# print(message)
+
+# prompt = "If you tell us who you are, we can personalize the messages you see."
+# prompt += "\nWhat is your first name? "
+# name = input(prompt) # input interprets everything as strings
+# print("\nHello, " + name + "!")
+
+# height = input("How tall are you, in meter? ")
+# height = float(height)
+# if height >= 1.7:
+#     print("\nYou're tall enough to ride!")
+# else:
+#     print("\nYou'll be able to ride when you're a little older.")
+
+# number = input("Enter a number, and I'll tell you if it's even or odd: ")
+# number = int(number)
+# if number % 2 == 0:
+#     print("\nThe number " + str(number) + " is even.")
+# else:
+#     print("\nThe number " + str(number) + " is odd.")
+
+## WHILE LOOPS
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
+
+# Using Flags
+active = True
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
+# Using break
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished.) "
+while True:
+    city = input(prompt)
+    if city == 'quit':
+        break
+    else:
+        print("I'd love to go to " + city.title() + "!")
+
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+      continue # Go back to the start of the loop
+    print(current_number)
+print('\n')
+
+# While with Dictionaries and Lists
+
+# Start with users that need to be verified,
+# and an empty list to hold confirmed users.
+unconfirmed_users = ['alice', 'brian', 'candace']
+confirmed_users = []
+# Verify each user until there are no more unconfirmed users.
+# Move each verified user into the list of confirmed users.
+while unconfirmed_users: # Runs until it's empty
+    current_user = unconfirmed_users.pop()
+    print("Verifying user: " + current_user.title())
+    confirmed_users.append(current_user)
+# Display all confirmed users.
+print("\nThe following users have been confirmed:")
+for confirmed_user in confirmed_users:
+    print(confirmed_user.title())
+
+print('\n')
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
+
+responses = {}
+# Set a flag to indicate that polling is active.
+polling_active = True
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+    # Store the response in the dictionary:
+    responses[name] = response
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
+# Polling is complete. Show the results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(name + " would like to climb " + response + ".")
